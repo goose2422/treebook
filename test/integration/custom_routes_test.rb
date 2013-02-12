c62a0a886261d68848c9717e7a0bfcca7de1fe3a
Todd Macey <tmmacey@gmail.com>
2013-02-12 15:52:42 -0500

    updated profile and added scoped status updates

--- test/integration/custom_routes_test.rb
+++ test/integration/custom_routes_test.rb
@@ -5,8 +5,13 @@ class CustomRoutesTest < ActionDispatch::IntegrationTest
   #   assert true
   # end
 
-  test "that the /login route opens the login page"
+  test "that the /login route opens the login page" do
   	get '/login'
   	assert_response: success
+	end
+
+	test "that a person's profile page works" do
+		get '/jimjim'
+		assert_response :success
 	end
 end
